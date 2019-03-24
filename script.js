0c2578e744f686fbd5fa4a2e0eb12734a9f31efd
carlisaac <c.isaac@lexsys.de>
2019-03-24 10:59:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * @author Carl
+ */
+
+function dropdownMenu() {
+	var x = document.getElementById("dropdownClick");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+		/* change topnav to topnav.responsive */
+	} else {
+		x.className = "topnav"
+	}
+	
+}
